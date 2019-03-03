57ca538e0be49a9553d940d3640c693dcd6ee5fe
Unknown <cayanportela@hotmail.com>
2019-03-02 21:08:44 -0300

    Script base_completa

--- torneios_estrut.R
+++ torneios_estrut.R
@@ -6,11 +6,13 @@ library(quantmod)
 library(QuantTools)
 library(TTR)
 
-vec <- c("AAPL","ABBV","ABT","ACN","AGN","AIG","ALL","AMGN","AMZN","AXP","BA","BAC","BIIB",
+vec <- c("AAPL","ABBV","ABT","ACN","AGN","AIG","ALL","AMGN","AMZN","AXP","BA","BAC",#"BIIB",
 "BK","BKNG","BLK","BMY",#"BRK.B",
-"C","CAT","CELG","CHTR","CL","CMCSA","COF","COP",
+"C","CAT","CELG",#"CHTR",
+"CL","CMCSA","COF","COP",
 "COST","CSCO","CVS","CVX","DHR","DIS","DUK","DWDP","EMR","EXC","F","FB","FDX","FOX",
-"FOXA","GD","GE","GILD","GM","GOOG","GOOGL","GS","HAL","HD","HON","IBM","INTC",
+"FOXA","GD","GE",#"GILD",
+"GM","GOOG","GOOGL","GS","HAL","HD","HON","IBM","INTC",
 "JNJ","JPM","KHC","KMI","KO","LLY","LMT","LOW","MA","MCD","MDLZ","MDT","MET","MMM",
 "MO","MRK","MS","MSFT","NEE","NFLX","NKE","NVDA","ORCL","OXY","PEP","PFE","PG",
 "PM","PYPL","QCOM","RTN","SBUX","SLB","SO","SPG","T","TGT","TXN","UNH","UNP","UPS",
