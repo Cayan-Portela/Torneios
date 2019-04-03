77fd8b9442c60297df68364626800e445d94a9e4
Unknown <cayanportela@hotmail.com>
2019-04-03 09:52:29 -0300

    Script redes

--- keras.R
+++ keras.R
@@ -1,4 +1,3 @@
-#library(mlbench)
 library(keras)
 library(dplyr)
 
@@ -6,25 +5,25 @@ library(dplyr)
 
 # Script inicial
 
-#base_completa_tudo_treino <- base_completa_tab1_treino
-#base_completa_tudo_teste  <- base_completa_tab1_teste
+set.seed(12345)
 
-x_train <- base_completa_tudo_treino[, - 126] %>% scale()   # YYY tab1 = 52   # which(colnames(base_completa_tudo_treino) == "YYY") --- 126
-x_test  <- base_completa_tudo_teste[, - 126] %>% scale()
+treino_id <- sample(x       = seq_len( nrow(base_completa2) ), 
+                    size    = floor(0.75*nrow(base_completa2)),
+                    replace = FALSE)
+  
+x_train <- base_completa2[treino_id, - 126] %>% scale()
+x_test  <- base_completa2[-treino_id, - 126] %>% scale()
 
-y_train <- ifelse( base_completa_tudo_treino[,126] == 1 , 1 , 0) %>%
-  as.matrix() %>%
-  to_categorical(y_train, num_classes = 2)
+y_train <- ifelse( base_completa2[treino_id,126] == 1 , 1 , 0) %>%
+           as.matrix()
+y_train <- to_categorical(y_train, num_classes = 2)
 
-y_test  <- ifelse( base_completa_tudo_teste[,126] == 1 , 1 , 0) %>%
-  as.matrix() %>%
-  to_categorical(y_train, num_classes = 2)
+y_test  <- ifelse( base_completa2[-treino_id,126] == 1 , 1 , 0) %>%
+           as.matrix()
+y_test  <- to_categorical(y_test, num_classes = 2)
 
 
 resultados <- list()
-
-##### -------------------- Cenario 1: --------------------- ###### 
-##### Tabela 1+2, No feature selection, Sigmoid, Dropout 0.3 #####
 
 # Criando objeto sequential model (MLP)
 
@@ -32,49 +31,52 @@ colunas <- list()
 
 colunas$todas <- colnames(x_train)
 
-colunas$lasso <- c("MFM","ADX","CHOSC","ADO","APO","AR_POS","AR_OSC","ATR","ATRP","AVOL","BB_BW","BWW","PERC_B",
-               "CCI","CMF","CVOL","ROC","COPP","DPO","DMI","DEMA","DSS","EMV","FORCE","HULL","MQO_ALPHA","MQO_STD",
-               "MACD","VOLUME","MASS","RMF","MFI","MIDPOINT","MOM","NVI","NATR","OBV","DS1","CHOPPINESS","PPO","PPOH",
-               "PVOH","PVI","KST","PSK","RSI","RVI","STRSI","STOCH_D","STOCH_K","STOCH_D_SLOW","TSI","ULCER","ULTOSC",
-               "VOOSC","PVOI","WILL_R","DISP","OSCP","PSY","DIU","BIAS","VOLR","ARATIO","BRATIO","REX","HPR",
-               "LPR","VMOM","VARR") 
+colunas$lasso <- c("MFM","ADL","ADX","ADO","APO","AR_POS","AR_OSC","ATRP","AVOL","BB_BW",
+                   "BWW","VOLAT","PERC_B","CCI","CVOL","CMO","ROC","COPP","DPO","DEMA","FORCE",
+                   "MQO_ALPHA","MQO_BETA","MACDH","MASS","RMF","MOM","NVI","NATR","SAR",
+                   "PVOH","RVI","STRSI","STOCH_K","STOCH_D_SLOW","TRIX","VOOSC","DISP",
+                   "OSCP","DIU","VOLR","BRATIO","REX","VARR")
 
-colunas$torneios <- c("KC_U","MAE_UP","RSI","CLOSE","KC_M","KC_L","OPEN","COPP","TRIX","TEMA","STRSI",
-                  "OSCP","FS1","MQO_BETA","MFM","PSY","SR1","FR1","DONCHIAN","STOCH_D_SLOW","WWS","BIAS",
-                  "DISP","AB_UP","WMA","FS2","SAR","WILL_R","SS1","APO","MQO_PRED","PD1","MOM","MACD","TRIMA",
-                  "TP","DR1","SR2","FR2","HPR","PPOH","MAE_LOW","MQO_ALPHA","EMA","STOCH_K","DID","MACDH",
-                  "PPO","DEMA","DS1")
+colunas$torneios <- c("KC_U","MQO_STD","FORCE","TP","EMA","LPR","PVOI","FR2","AVOL","SS2",
+                    "CLOSE","NATR","BB_BW","FS1","ATRP","PERC_B","STOCH_D_SLOW","ADX",
+                    "MASS","STRSI","WWS","TEMA","ARATIO","VOLAT","AR_OSC","MIDPOINT",
+                    "MACDH","TRIX","MACD","VWAP","APO","MOM","BIAS","ULTOSC","DSS","PPOH",
+                    "RMF","VOLR","DISP","MFM","WILL_R","CMF","TSI","VOLUME","SR2","VARR","FS2",
+                    "DPO","DEMA","DS1")
 
 
 
-colunas$stepwise <- c("MFM", "BIAS","PSY","STOCH_D","STOCH_K","STOCH_D_SLOW","ULTOSC","PPOH","CCI","VOLR",
-                     "DIU", "MFI","BRATIO","DISP","NVI","BWW","PERC_B","EMV","COPP","MACD","ROC","OSCP","MASS",
-                     "VOLUME", "ADX","CHOSC","PVOH","CHOPPINESS","PVO","DMI","RVI","MACDH","PPO","REX","MOM",
-                     "VOOSC", "APO","DSS","ADL","HULL","OBV","VMOM","MPP","VARR","ATR","VOLAT","AD","MIDPRICE",
-                     "ARATIO", "MQO_STD")
+colunas$stepwise <- c("DPO","DISP","VOLAT","MIDPOINT","PPOH","DSS","PVI","AD","RVI",
+                      "HULL","NVI","DMI","STOCH_D_SLOW","BRATIO","BWW","EMV","ADX",
+                      "ATRP","VOOSC","NATR","VARR","HPR","PSK","CHOSC","AR_OSC","RSI",
+                      "MASS","KAMA","MQO_STD","VWAP","AR_POS","AB_DOWN","MQO_BETA",
+                      "AR_NEG","APO","CHAND_SHORT","VAMA","BB_UP","BB_LOW","ROC","MFM",
+                      "FORCE","PC_UP","MQO_ALPHA","PC_DOWN","DIU","MQO_PRED","ATR", "AVOL","KST")
 
-i = 1
-k = 'sigmoid'
-j = 0
-    for (i in 1:4){
+
+# i = 4
+# k = 'sigmoid'
+# j = 0.3
+
+system.time(
+    for (i in 1:4) {
       
-      colunas_sel <- which( colnames(base_completa_tudo_treino) %in% colunas[[i]] )
-     
-        for (k in c('sigmoid','relu') ){
+      colunas_sel <- which( colnames(base_completa2) %in% colunas[[i]] )
           
-          for(j in c(0.3,0) ){
+          for(j in c(0.3,0) ) {
             
               model <- keras_model_sequential() 
               
               # Definindo arquitetura da rede
               model %>% 
-                    layer_dense(units = 15, activation = k, input_shape = length(colunas[[i]]) ) %>% 
+                    layer_dense(units = 15, activation = 'sigmoid', 
+                                input_shape = length(colunas[[i]]) ) %>% 
                     layer_dropout(rate = j) %>% 
-                    layer_dense(units = 15, activation = k) %>%
+                    layer_dense(units = 15, activation = 'sigmoid') %>%
                     layer_dropout(rate = j) %>%
-                    layer_dense(units = 15, activation = k) %>%
+                    layer_dense(units = 15, activation = 'sigmoid') %>%
                     layer_dropout(rate = j) %>%
-                    layer_dense(units = 15, activation = k) %>%
+                    layer_dense(units = 15, activation = 'sigmoid') %>%
                     layer_dropout(rate = j) %>%
                     layer_dense(units = 2, activation = 'sigmoid')
     
@@ -83,7 +85,9 @@ j = 0
                     compile(
                       loss = 'categorical_crossentropy',
                       optimizer = optimizer_rmsprop(),
-                      metrics = c('accuracy')
+                      metrics = c('accuracy',
+                                  'binary_accuracy',
+                                  'categorical_accuracy')
                     )
     
               # Treina o modelo. (Definir outro epochs e batch_size ?)
@@ -93,36 +97,49 @@ j = 0
                 validation_split = 0.2
               )
     
-              plot(history)
+              #plot(history)
               
      # Accuracy = Avaliacao na base teste. Esse valor de acuracia que utilizaremos no paper.
               resultados[[ length(resultados) + 1 ]] <- data.frame(Variables  = names(colunas[i]),
                                                                    Hidden_layers = 3,
                                                                    Dropout    = j,
-                                                                   Activation = k,
+                                                                   Activation = 'sigmoid',
                                                                    Accuracy_in = history$metrics$val_acc[[400]],
                                                                    Accuracy_out   = model %>% 
                                                                                  evaluate(x_test[ , colunas_sel ], y_test) %>% 
-                                                                                 .$acc
+                                                                                 .$acc,
+                                                                   Binary_acc_out = model %>% 
+                                                                                    evaluate(x_test[ , colunas_sel ], y_test) %>% 
+                                                                                    .$binary_accuracy,
+                                                                   Categ_acc_out  = model %>% 
+                                                                                    evaluate(x_test[ , colunas_sel ], y_test) %>% 
+                                                                                    .$categorical_accuracy
                                                                    )
-              
-        
+          
           }
-        }
+ 
     }
+    
+)
+
+
 
 ##### -------------------- Fim Cenario 1: --------------------- ###### 
 
+tabela_1 <- do.call( rbind, resultados )
 
+write.csv(tabela_1, file = "tabela_1.csv", row.names = F)
+save(tabela_1, file = 'tabela_1.RData')
 
 #### ----- Cenario 2: 5 camadas ------------ #####
+rm(resultados)
+resultados <- list()
+
 system.time(
 
 for (i in 1:4){
   
-  colunas_sel <- which( colnames(base_completa_tudo_treino) %in% colunas[[i]] )
-  
-  for (k in c('sigmoid','relu') ){
+  colunas_sel <- which( colnames(base_completa2) %in% colunas[[i]] )
     
     for(j in c(0.3,0) ){
       
@@ -130,17 +147,17 @@ for (i in 1:4){
       
       # Definindo arquitetura da rede
       model %>% 
-        layer_dense(units = 15, activation = k, input_shape = length(colunas[[i]]) ) %>% 
+        layer_dense(units = 15, activation = 'sigmoid', input_shape = length(colunas[[i]]) ) %>% 
         layer_dropout(rate = j) %>% 
-        layer_dense(units = 15, activation = k) %>%
+        layer_dense(units = 15, activation = 'sigmoid') %>%
         layer_dropout(rate = j) %>%
-        layer_dense(units = 15, activation = k) %>%
+        layer_dense(units = 15, activation = 'sigmoid') %>%
         layer_dropout(rate = j) %>%
-        layer_dense(units = 15, activation = k) %>%
+        layer_dense(units = 15, activation = 'sigmoid') %>%
         layer_dropout(rate = j) %>%
-        layer_dense(units = 15, activation = k) %>%
+        layer_dense(units = 15, activation = 'sigmoid') %>%
         layer_dropout(rate = j) %>%
-        layer_dense(units = 15, activation = k) %>%
+        layer_dense(units = 15, activation = 'sigmoid') %>%
         layer_dropout(rate = j) %>%
         layer_dense(units = 2, activation = 'sigmoid')
       
@@ -149,7 +166,9 @@ for (i in 1:4){
         compile(
           loss = 'categorical_crossentropy',
           optimizer = optimizer_rmsprop(),
-          metrics = c('accuracy')
+          metrics = c('accuracy',
+                      'binary_accuracy',
+                      'categorical_accuracy')
         )
       
       # Treina o modelo. (Definir outro epochs e batch_size ?)
@@ -159,22 +178,25 @@ for (i in 1:4){
         validation_split = 0.2
       )
       
-      plot(history)
+      #plot(history)
       
       # Accuracy = Avaliacao na base teste. Esse valor de acuracia que utilizaremos no paper.
       resultados[[ length(resultados) + 1 ]] <- data.frame(Variables  = names(colunas[i]),
                                                            Hidden_layers = 5,
                                                            Dropout    = j,
-                                                           Activation = k,
+                                                           Activation = 'sigmoid',
                                                            Accuracy_in = history$metrics$val_acc[[400]],
                                                            Accuracy_out   = model %>% 
                                                              evaluate(x_test[ , colunas_sel ], y_test) %>% 
-                                                             .$acc
+                                                             .$acc,
+                                                           Binary_acc_out = model %>% 
+                                                             evaluate(x_test[ , colunas_sel ], y_test) %>% 
+                                                             .$binary_accuracy,
+                                                           Categ_acc_out  = model %>% 
+                                                             evaluate(x_test[ , colunas_sel ], y_test) %>% 
+                                                             .$categorical_accuracy
       )
-      
-      
     }
-  }
 }
 
 
@@ -191,6 +213,8 @@ save(tabela_2, file = 'tabela_2.RData')
 
 
 ##### -------------------- Cenario 3: 7 camadas --------------------- ###### 
+rm(resultados)
+resultados <- list()
 
 system.time(
   
@@ -229,7 +253,9 @@ system.time(
           compile(
             loss = 'categorical_crossentropy',
             optimizer = optimizer_rmsprop(),
-            metrics = c('accuracy')
+            metrics = c('accuracy',
+                        'binary_accuracy',
+                        'categorical_accuracy')
           )
         
         # Treina o modelo. (Definir outro epochs e batch_size ?)
@@ -239,17 +265,23 @@ system.time(
           validation_split = 0.2
         )
         
-        plot(history)
+        #plot(history)
         
         # Accuracy = Avaliacao na base teste. Esse valor de acuracia que utilizaremos no paper.
         resultados[[ length(resultados) + 1 ]] <- data.frame(Variables  = names(colunas[i]),
                                                              Hidden_layers = 7,
                                                              Dropout    = j,
-                                                             Activation = k,
+                                                             Activation = 'sigmoid',
                                                              Accuracy_in = history$metrics$val_acc[[400]],
                                                              Accuracy_out   = model %>% 
                                                                evaluate(x_test[ , colunas_sel ], y_test) %>% 
-                                                               .$acc
+                                                               .$acc,
+                                                             Binary_acc_out = model %>% 
+                                                               evaluate(x_test[ , colunas_sel ], y_test) %>% 
+                                                               .$binary_accuracy,
+                                                             Categ_acc_out  = model %>% 
+                                                               evaluate(x_test[ , colunas_sel ], y_test) %>% 
+                                                               .$categorical_accuracy
         )
         
         
